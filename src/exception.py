4f6a6db947e807b8d132b6dbda04c02e3ec224da
Guliev Nadir <gulievnadir3@gmail.com>
2024-09-19 11:06:29 +0300

    fix: error message

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_details(error, error_data:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_details(self.error_message, error_detail)
+        self.error_message = error_message_details(error_message, error_detail)
     
     def __str__(self):
         return self.error_message
